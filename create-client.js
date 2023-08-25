bd740514b933b4d813825cd396672780b3f9f3a4
Alex <alex@Alexs-MacBook-Air.local>
2023-08-25 15:23:12 +0300

    test

--- create-client.js
+++ create-client.js
@@ -9,13 +9,7 @@ exports.handler = async (event, context) => {
 
             const requestBody = JSON.parse(event.body);
             const apiKey = process.env.COMPLYCUBE_API_KEY;
-            console.log("APIKEY" + apiKey);
             const url = 'https://api.complycube.com/v1/clients';
-            const requestHeaders = {
-                'Authorization': "test_a2FIMmpZY0tEaWFLODg2OEU6ODRhMThhZDkyMDljZDUzNDYwNmEyZTc4NWVlYjE4ODg1YTY3OGQ2MGMzZWE0YTI1MDg2NmIyMTRjYmI0OTVmNA==",
-                'Content-Type': 'application/json'
-            };
-
             let myHeaders = new Headers();
             myHeaders.append("Content-Type", "application/json");
             myHeaders.append("Authorization", 'test_a2FIMmpZY0tEaWFLODg2OEU6ODRhMThhZDkyMDljZDUzNDYwNmEyZTc4NWVlYjE4ODg1YTY3OGQ2MGMzZWE0YTI1MDg2NmIyMTRjYmI0OTVmNA==');
@@ -27,43 +21,19 @@ exports.handler = async (event, context) => {
             });
             if (res.ok) {
                 const data = await res.json();
-                console.log(data);
+
                 return {
                     statusCode: 200,
-                    body: 'worked'
+                    body: data
                 };
             } else {
                 const err = await res.json();
                 console.log(err);
                 return {
                     statusCode: 501,
-                    body: 'ERR'
+                    body: err
                 };
             }
-
-            // fetch(url, {
-            //     method: 'POST',
-            //     headers: requestHeaders,
-            //     body: JSON.stringify(requestBody)
-            // }).then(response => response.json())
-            //   .then(result => {
-            //         return {
-            //             statusCode: 200,
-            //             body: result,
-            //         }
-            //     })
-            //   .catch(error => {
-            //         return {
-            //             statusCode: 500,
-            //             body: error.getError().body
-            //         };
-            //     });
-
-
-            return {
-                statusCode: 500,
-                body: 'WTF'
-            };
 
         } catch (error) {
             // Return an error response if there was an issue processing the request
