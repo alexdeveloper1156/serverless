13af1d62a86d7dacce3ea8ed19105a4ec9abafc7
Alex <alex@Alexs-MacBook-Air.local>
2023-08-27 12:39:38 +0300

    asd

--- create-token.js
+++ create-token.js
@@ -1,4 +1,15 @@
 exports.handler = async (event, context) => {
+    if (event.httpMethod === "OPTIONS") {
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "GET, POST, OPTION",
+            },
+        };
+    }
+
     if (event.httpMethod === 'POST') {
         try {
             const requestBody = JSON.parse(event.body);
