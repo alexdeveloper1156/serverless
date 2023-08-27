7038d520a87139aea0eb88358b10d4a889af8c78
Alex <alex@Alexs-MacBook-Air.local>
2023-08-27 12:25:54 +0300

    asda

--- create-client.js
+++ create-client.js
@@ -14,6 +14,7 @@ exports.handler = async (event, context) => {
 
     if (event.httpMethod === 'POST') {
         try {
+            console.log('{POST!');
             // Add CORS headers
             const headers = {
                 'Access-Control-Allow-Origin': '*', // Replace * with the appropriate domain
@@ -36,22 +37,38 @@ exports.handler = async (event, context) => {
 
                 return {
                     statusCode: 200,
-                    body: JSON.stringify(data)
+                    body: JSON.stringify(data),
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Headers": "Content-Type",
+                        "Access-Control-Allow-Methods": "GET, POST, OPTION",
+                    },
                 };
             } else {
                 const err = await res.json();
                 console.log(err);
                 return {
                     statusCode: 501,
-                    body: JSON.stringify(err)
+                    body: JSON.stringify(err),
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Headers": "Content-Type",
+                        "Access-Control-Allow-Methods": "GET, POST, OPTION",
+                    },
                 };
             }
 
         } catch (error) {
+            console.log(error);
             // Return an error response if there was an issue processing the request
             return {
                 statusCode: 500,
                 body: JSON.stringify({ error: 'Failed to process GET request' }),
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Methods": "GET, POST, OPTION",
+                },
             };
         }
     }
