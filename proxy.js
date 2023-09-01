781a70450bc9ec9bf911fe71e786db4c7909fd5f
Alex <alex@Alexs-MacBook-Air.local>
2023-09-01 15:28:31 +0300

    fixed

---
+++ proxy.js
@@ -0,0 +1,69 @@
+exports.handler = async (event, context) => {
+    if (event.httpMethod === "OPTIONS") {
+        return {
+            statusCode: 200,
+            headers: {
+                "Access-Control-Allow-Origin": "*",
+                "Access-Control-Allow-Headers": "Content-Type",
+                "Access-Control-Allow-Methods": "GET, POST, OPTION",
+            },
+        };
+    }
+
+    if (event.httpMethod === 'POST') {
+        try {
+            console.log(event.body);
+            const requestBody = JSON.parse(event.body);
+            const apiKey = process.env.COMPLYCUBE_API_KEY;
+            const url = 'http://164.90.142.76/api/flood-insurance-api';
+            let myHeaders = new Headers();
+                myHeaders.append("Content-Type", "application/json");
+
+            const res = await fetch(url,{
+                method: 'POST',
+                headers: myHeaders,
+                data: {
+                    clientId: requestBody.clientId,
+                }
+            });
+            if (res.ok) {
+                const data = await res.json();
+
+                return {
+                    statusCode: 200,
+                    body: JSON.stringify(data),
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Headers": "Content-Type",
+                        "Access-Control-Allow-Methods": "GET, POST, OPTION",
+                    },
+                };
+            } else {
+                const err = await res.json();
+                console.log(err);
+                return {
+                    statusCode: 501,
+                    body: JSON.stringify(err),
+                    headers: {
+                        "Access-Control-Allow-Origin": "*",
+                        "Access-Control-Allow-Headers": "Content-Type",
+                        "Access-Control-Allow-Methods": "GET, POST, OPTION",
+                    },
+                };
+            }
+
+        } catch (error) {
+            console.log(error);
+            // Return an error response if there was an issue processing the request
+            return {
+                statusCode: 500,
+                body: JSON.stringify({ error: 'Failed to process GET request' }),
+                headers: {
+                    "Access-Control-Allow-Origin": "*",
+                    "Access-Control-Allow-Headers": "Content-Type",
+                    "Access-Control-Allow-Methods": "GET, POST, OPTION",
+                },
+            };
+        }
+    }
+};
