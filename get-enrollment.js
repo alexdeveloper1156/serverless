94ea8801d67c655ab77df03341aee54f3cb69f07
Alex <alex@Alexs-MacBook-Air.local>
2023-09-06 16:38:52 +0300

    asda

--- get-enrollment.js
+++ get-enrollment.js
@@ -15,7 +15,6 @@ exports.handler = async (event, context) => {
             console.log(event.body);
             const requestBody = JSON.parse(event.body);
             const apiKey = process.env.HUBSPOT_PAT;
-            console.log(apiKey);
             const url = 'https://api.hubapi.com/crm/v3/objects/enrollment/search';
             let myHeaders = new Headers();
                 myHeaders.append("Content-Type", "application/json");
@@ -28,15 +27,15 @@ exports.handler = async (event, context) => {
                       {
                         propertyName: 'person_primary_email',
                         operator: 'EQ',
-                        value: requestBody.email || 'ravins@jasbhi.com', // I dont know why... 
+                        value: requestBody.email, // I dont know why...
                       },
                     ],
                   },
                 ],
                 properties: ['sku', 'person_primary_kyc_status', 'notary', 'is_accredited', 'accreditation'],
               }
-            );;
-
+            );
+            console.log(requestData);
             const res = await fetch(url,{
                 method: 'POST',
                 data: requestData,
